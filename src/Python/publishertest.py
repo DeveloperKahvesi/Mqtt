be03a536887854227498a53f6e3ffa7c15a20239
Baris AKIN <barisakin@gmail.com>
2019-10-14 13:51:05 +0300

    python

--- src/Python/publishertest.py
+++ src/Python/publishertest.py
@@ -8,7 +8,7 @@ client = mqtt.Client()
 client.username_pw_set(username="testuser",password="testpass")
 client.connect("www.baltavista.com", 8883)
 
-client.publish("test","hello sesdfsdf")
+client.publish("test","hello server")
 
 client.loop_forever()
  
